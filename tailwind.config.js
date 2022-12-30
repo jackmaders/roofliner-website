04d61c0bd292ccf73684b0ed16ff688431a8372c
Jack Maders <jackwmaders@gmail.com>
2022-12-30 17:45:36 +0000

    added background image styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
       zIndex: {
         NAVBAR: "2",
       },
+      backgroundImage: {
+        "header-background":
+          "linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6) ), url('@/assets/images/header-background.webp')",
+        "shingle-tiles":
+          "linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('@/assets/images/shingle-tiles.webp')",
+        "slate-tiles":
+          "linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('@/assets/images/slate-tiles.webp')",
+        "velux-window":
+          "linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('@/assets/images/velux-window.webp')",
+      },
     },
   },
   plugins: [],
