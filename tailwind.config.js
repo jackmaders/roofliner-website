81bdba03acbac50f7f1cfd6e83e5ad55e0bf8aad
Jack Maders <jackwmaders@gmail.com>
2022-12-30 17:45:39 +0000

    adding extra small width breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /* eslint-env node */
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      screens: { xs: "425px", ...defaultTheme.screens },
       zIndex: {
         NAVBAR: "2",
         MODAL: "3",
