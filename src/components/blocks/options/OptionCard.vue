e6dbe90a709206564eeb1c21b830663dd2662b63
Jack Maders <jackwmaders@gmail.com>
2022-12-30 17:45:40 +0000

    better text size for option cards

--- src/components/blocks/options/OptionCard.vue
+++ src/components/blocks/options/OptionCard.vue
@@ -3,7 +3,7 @@
     class="flex items-center justify-center p-2 transition-all bg-no-repeat bg-cover rounded-2xl aspect-square hover:scale-110"
   >
     <span
-      class="text-2xl font-bold text-center text-white uppercase text-shadow-lg"
+      class="text-xl font-bold text-center text-white uppercase xs:text-2xl text-shadow-lg"
     >
       <slot></slot>
     </span>
